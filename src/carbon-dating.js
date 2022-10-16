f429501d6072888bc35500234f12359bb3451f17
wasek07 <wasek07@ya.ru>
2022-10-17 00:29:39 +0200

    feat: add solution carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,28 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  var k = 0.693 / HALF_LIFE_PERIOD
+  if (typeof sampleActivity !== 'string') {
+    return false
+  }
+  if (sampleActivity > 0) {
+    var years = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k)
+    if (years > 0) {
+      return years
+    } else {
+      return false
+    }
+  } else {
+    return false
+  }
 }
 
 module.exports = {
   dateSample
-};
+}
+
+
+
+
+
